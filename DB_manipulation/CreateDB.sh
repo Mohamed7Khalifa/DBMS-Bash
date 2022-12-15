3284848b1866b99907f4cc6db11ca2404bf11f46
Zein <ahmedzein8@gmail.com>
2022-12-15 12:11:13 +0200

    not done yet

---
+++ DB_manipulation/CreateDB.sh
@@ -0,0 +1,8 @@
+read -p "Please Enter your database name: " DB_name 
+
+if [[ -d ~/DataBase/$DB_name ]] ; then
+    echo "The database $DB_name is already exist try diffrent name. " 
+else 
+    mkdir ~/DataBase/$DB_name
+    echo "Database created "
+fi 
