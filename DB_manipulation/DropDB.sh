71af8edb481d5dea1cae928237eb537838cfe709
AhmedZeins <AhmedZein8@gmail.com>
2022-12-15 19:08:15 +0200

    still working on dropDB

--- DB_manipulation/DropDB.sh
+++ DB_manipulation/DropDB.sh
@@ -2,9 +2,13 @@
 read -p "Please Enter your database name: " DB_name 
 
 if [[ -d ~/DataBase/$DB_name ]]; then
-    
+    read "Are you sure you want to delete this Database? y/n" sure 
+    if [[ $sure=y ]] ; then
     rm -r $DB_name
     echo "The database $DB_name is deleted  "
+    else 
+        echo "okay"
+    fi 
 
 else 
     echo "write the right db name "
