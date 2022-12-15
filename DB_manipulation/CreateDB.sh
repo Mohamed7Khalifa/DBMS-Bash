f311a7788966eb1b881958c9922ec90538b638ea
AhmedZeins <Ahmedzein8@gmail.com>
2022-12-15 14:56:36 +0200

    still working on it

--- DB_manipulation/CreateDB.sh
+++ DB_manipulation/CreateDB.sh
@@ -4,7 +4,7 @@ read -p "Please Enter your database name: " DB_name
 if [[ -d ~/DataBase/$DB_name ]]; then
     echo "The database $DB_name is already exist try diffrent name. " 
 
-elif [[ $DB_name=""]]; then
+elif [[ $DB_name="" ]]; then
     echo "please try again & enter a name "
 else 
     mkdir ~/DataBase/$DB_name
