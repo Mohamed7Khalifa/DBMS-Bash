5b0593484103e877531ac2c6658564a4ec03de06
Mohamed7Khalifa <MHMD7Khalifa@gmail.com>
2022-12-22 16:51:26 +0200

    done insert file

--- Tables_manipulation/Insert.sh
+++ Tables_manipulation/Insert.sh
@@ -8,11 +8,69 @@ do
     read tbName
 done
 if [[ -f ~/DataBase/$1/$tbName ]] ; then
-    awk -f | '
-    {
-        
-    }
-    ' ~/DataBase/$1/$tbName
+    columnsNum=`awk '
+        BEGIN{
+            FS="|"
+        }
+        END{
+            print NR
+        }
+        ' ~/DataBase/$1/.$tbName`
+    # echo $columnsNum
+    counter=2
+    while [[ $counter -le $columnsNum ]]
+    do
+    columnName=`awk '
+        BEGIN{
+            FS="|"
+        }
+        {
+            if(NR=='$counter') print $1
+        }
+        ' ~/DataBase/$1/.$tbName`
+    columnType=`awk '
+        BEGIN{
+            FS="|"
+        }
+        {
+            if(NR=='$counter') print $2
+        }
+        ' ~/DataBase/$1/.$tbName`
+    columnKey=`awk '
+        BEGIN{
+            FS="|"
+        }
+        {
+            if(NR=='$counter') print $3
+        }
+        ' ~/DataBase/$1/.$tbName`
+    # echo $columnName
+    # echo $columnType
+    # echo $columnKey
+    while [[ true ]]
+    do
+        read -p "Enter value of $columnName ($columnType) = "  input
+        if [[ columnType='int' && $input = +([0-9]) || columnType='varchar' && $input = +([a-zA-Z0-9]) ]] ;then
+            if [[ $counter == $columnsNum ]] ; then
+                echo $input >> ~/DataBase/$1/$tbName
+            else
+                echo -n $input'|' >> ~/DataBase/$1/$tbName 
+            fi
+            break
+        else
+            echo "enter valid value X( "
+        fi
+    done
+    # echo -e "\n" >> ~/DataBase/$1/$tbName
+    (( counter++ ))
+    done
+    if [[ $? == 0 ]] ; then
+        echo "Data Inserted Successfully"
+        ./main.sh
+    else
+        echo "Error Inserting Data into Table $tbName"
+        ./Tables_manipulation/TbMenu.sh 
+    fi
 
 else
     echo 'sorry this table is not exist X('
