41bb691c84eeef50436ac5d46ac1cd0b92250c81
Mohamed7Khalifa <MHMD7Khalifa@gmail.com>
2022-12-25 21:36:54 +0200

    fix drop table function

--- Tables_manipulation/DropTb.sh
+++ Tables_manipulation/DropTb.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-read -p "Please Enter your Table name: " tbName
 DB_name=$1
 echo "The available Tables are: "
 echo "---------------------------------"
 ls -F ~/DataBase/$DB_name/ | grep -v "/"
 echo "---------------------------------"
+read -p "Please Enter your Table name: " tbName
 sleep 1
-if [[ -d ~/DataBase/$DB_name/$tbName ]]; then
+if [[ -f ~/DataBase/$DB_name/$tbName ]]; then
     read -p  "Are you sure you want to delete this Table? y/n : " sure 
     case $sure in 
-     Yy]* )
+     [Yy]* )
         rm -r ~/DataBase/$DB_name/$tbName
         rm -r ~/DataBase/$DB_name/.$tbName
         echo "Table has been deleted "
+        ./Tables_manipulation/TbMenu.sh
+
         ;;
     [Nn]* )
         echo "canceled "
+        ./Tables_manipulation/TbMenu.sh
+
         ;;
     * )
         echo "please enter y or n only "
